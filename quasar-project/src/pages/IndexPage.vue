a3c6bb65fe2f50debac50325ca4071c2c2761ed9
towro983 <towro983@student.otago.ac.nz>
2025-03-01 01:09:12 +1300

    Changed back to non-nested array
    should make analytics easier

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -5,31 +5,28 @@
     <q-btn label="Start Timer" @click="startTimer" />
     <q-btn label="Stop Timer" @click="stopTimer" />
     <q-input v-model="exerciseName" type="text" label="Exercise" />
-    <q-btn label="Add" @click="addExercise(exerciseName)" />
     <p>Timer: {{ formattedTime }}</p>
     <q-card> Exercise Name </q-card>
     <q-card-section>
-      <div v-for="exercise in exercises" v-bind:key="exercise.name">
-        <p>{{ exercise.name }}</p>
-        <div v-for="(set, index) in exercise.sets" v-bind:key="index">
-          <q-input
-            v-model="set.repCount"
-            type="number"
-            label="Reps"
-            min="0"
-            step="1"
-            @update:model-value="saveToLocalStorage"
-          />
-          <q-input
-            v-model="set.weight"
-            type="number"
-            label="Weight (kg)"
-            min="0"
-            @update:model-value="saveToLocalStorage"
-          />
-          <q-btn :icon="matDelete" label="Remove Set" @click="removeSet(exercise, index)" stack />
-        </div>
-        <q-btn :icon="matAdd" label="New Set" @click="addSet(exercise)" stack />
+      <q-btn :icon="matAdd" @click="addSet(exerciseName)" />
+      <div v-for="(set, index) in sets" v-bind:key="index">
+        <p>{{ set.exerciseName }}</p>
+        <q-input
+          v-model="set.reps"
+          type="number"
+          label="Reps"
+          min="0"
+          step="1"
+          @update:model-value="saveToLocalStorage"
+        />
+        <q-input
+          v-model="set.weight"
+          type="number"
+          label="Weight (kg)"
+          min="0"
+          @update:model-value="saveToLocalStorage"
+        />
+        <q-btn :icon="matDelete" @click="deleteSet(index)" />
       </div>
     </q-card-section>
   </q-page>
@@ -40,10 +37,38 @@ import { matDelete, matAdd } from '@quasar/extras/material-icons';
 
 import { ref, computed, onMounted } from 'vue';
 
+const sets = ref([]);
 const time = ref(0);
-const exerciseName = ref('');
-const exercises = ref([]); // the exercises of the current workout.
 const interval = ref(null);
+const exerciseName = ref('');
+
+const addSet = (exercise) => {
+  sets.value.push({ exerciseName: exercise, reps: '', weight: '' });
+  saveToLocalStorage();
+};
+
+const deleteSet = (index) => {
+  sets.value.splice(index, 1);
+  saveToLocalStorage();
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('sets', JSON.stringify(sets.value));
+};
+
+const startWorkout = () => {
+  startTimer();
+  sets.value = []; // as a new workout is started, discard the current exercises from the previous workout
+};
+
+const stopWorkout = () => {
+  stopTimer();
+
+  // save the sets to localStorage
+
+  time.value = 0;
+  sets.value = [];
+};
 
 const startTimer = () => {
   if (interval.value) clearInterval(interval.value);
@@ -55,49 +80,6 @@ const startTimer = () => {
 
 const stopTimer = () => {
   clearInterval(interval.value);
-};
-
-const startWorkout = () => {
-  startTimer();
-  exercises.value = []; // as a new workout is started, discard the current exercises from the previous workout
-};
-
-const stopWorkout = () => {
-  stopTimer();
-  // const currWorkout = {
-  //   date: new Date().toLocaleString(),
-  //   duration: time.value,
-  //   exercises: exercises.value,
-  // };
-  // localStorage.setItem('workouts', JSON.stringify(currWorkout));
-  // add currWorkout to list of previous workouts
-
-  // reset time and exercises to prepare for the next workout
-  exercises.value = [];
-  time.value = 0;
-};
-
-const saveToLocalStorage = () => {
-  localStorage.setItem('exercises', JSON.stringify(exercises.value));
-};
-
-// if there are saved sets in localStorage, use those, otherwise start with 1 blank set
-// const sets = ref(JSON.parse(localStorage.getItem('exercises')) || [{}]);
-
-const addExercise = () => {
-  exercises.value.push({ name: exerciseName.value, sets: [] });
-  exerciseName.value = '';
-  saveToLocalStorage();
-};
-
-const addSet = (exercise) => {
-  exercise.sets.push({ repCount: '', weight: '' });
-  saveToLocalStorage();
-};
-
-const removeSet = (exercise, index) => {
-  exercise.sets.splice(index, 1);
-  saveToLocalStorage();
 };
 
 function formatTimeUnit(unit) {
@@ -114,11 +96,10 @@ function formatTime(totalSeconds) {
 
 const formattedTime = computed(() => formatTime(time.value));
 
-// when page loads, attempts to fetch saved sets from storage. If successful, use these sets
 onMounted(() => {
-  const savedExercises = localStorage.getItem('exercises');
-  if (savedExercises) {
-    exercises.value = JSON.parse(savedExercises);
+  const savedSets = localStorage.getItem('sets');
+  if (savedSets) {
+    sets.value = JSON.parse(savedSets);
   }
 });
 </script>
