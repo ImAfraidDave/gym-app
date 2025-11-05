ae7fe4e17d8a0ff300b89d430ee6f08a565b917c
Romili Townsend <162295450+ImAfraidDave@users.noreply.github.com>
2025-11-05 15:14:19 +1300

    Add useAPI option to storageFactory
    Added option to use API for storage

--- quasar-project/src/composables/storageFactory.js
+++ quasar-project/src/composables/storageFactory.js
@@ -1,6 +1,8 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useAPI } from './useAPI';
 
 export function useStorageFactory() {
   // uncomment whichever storage option to use, comment all others
   return useLocalStorage();
+  // return useAPI();
 }
